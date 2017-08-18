06e3cfa99aa47f40248fc1fe585e63670f771fe9
magcurly <31119028+magcurly@users.noreply.github.com>
2017-08-18 16:02:31 +0800

    Update run_LDA.pl

--- run_LDA.pl
+++ run_LDA.pl
@@ -3,18 +3,15 @@ use strict;
 use warnings;
 use English;
 
-my $Rscript = "/hwfssz1/ST_META/PN/zhujiahui/R/bin/Rscript";
+my $Rscript = "/path/to/your/Rscript"; ##or you can add a parameter below.
 
-my ($train_set,$test_set,$train_group,$test_group,$out)=@ARGV;
+my ($train_set,$test_set,$train_group,$test_group,$out)=@ARGV; ##you can change it with Getopt::Long & FindBin qw($Bin)
 
+my $help = "perl run_LDA.pl train.set test.set train.group test.group prefix";
+if(!$train_set && !$test_set && !$train_group && !$test_group && !$out){die "$help";}
+## These two line above were not in my original script.
 LDA:{
 	open ERR,"$Rscript /hwfssz1/ST_META/PN/zhujiahui/meta-analysis/test-cOMG/LDA.R $train_set $test_set $train_group $test_group $out 2>&1|";
-
-#my $count=`wc -l err.or`;
-#$count =~ /^\d+/;
-#$count = $MATCH;
-#if($count == 4){last LDA;}
-#open ERR,"err.or";
 	my @err;my $count=0;
 	while(<ERR>){
 		chomp;
@@ -25,12 +22,12 @@ LDA:{
 			@err=split /\s+/,$num;
 		}
 	}
-	print join "\t",@err;
+	#print join "\t",@err;
 	close ERR;
 	if($count!=4){last LDA;}
 	open IN1,$train_set;
 	open IN2,$test_set;
-	open OUT1,">$train_set.out";
+	open OUT1,">$train_set.out"; 
 	open OUT2,">$test_set.out";
 	my ($c,$i)=(0,0);
 	my $head1=<IN1>;
@@ -52,6 +49,7 @@ LDA:{
 	close OUT1;close OUT2;
 	$train_set.=".out";
 	$test_set.=".out";
-#`rm err.or`;
 	redo LDA;
 }
+`rm *.out`;
+#I did not put the entire file into the swap because files I used to process had millions of lines and variables. Whether there is a better way to remove uncapable variables haven't come to my mind. If some brilliant minds find out how to improve it, please let me know.
