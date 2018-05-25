d8e8cb663759be5f7439052bfec148f48bf1edc8
Margaret Chu <31119028+magcurly@users.noreply.github.com>
2018-05-25 11:04:09 +0800

    Update acc2TaxaSQL.R

--- nt_blast_anno/acc2TaxaSQL.R
+++ nt_blast_anno/acc2TaxaSQL.R
@@ -1,3 +1,9 @@
-#Build a SQL database before performing the annotation
+#Build an ass2tax SQL database before performing the annotation
 library("taxonomizr")
-read.accession2taxid(list.files('/hwfssz1/ST_META/PN/zhujiahui/bacteria-gene/script/nt','accession2taxid.gz$'),'accessionTaxa.sql')
+read.accession2taxid(list.files('/Path/to/','accession2taxid.gz$'),'/path/to/your/accessionTaxa.sql')
+
+#The directory must contain 4 files to build the sql database as following: 
+#nucl_est.accession2taxid.gz
+#nucl_gb.accession2taxid.gz
+#nucl_gss.accession2taxid.gz
+#nucl_wgs.accession2taxid.gz
